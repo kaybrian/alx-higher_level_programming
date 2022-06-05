afddd6b8fa351127833e2fb808fa9d54e8e678ab
Kayongo brian <kayonbrian@gmail.com>
2022-06-05 13:02:08 +0300

    writng new strinf without the c or C

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    def no_c(my_string):
+        strn = ""
+        for c in my_string:
+            if c != 'c' and c != 'C':
+                strn += c
+        return strn
