e7a5480dab14684b09552dc877fdf8ddc3a68484
Kayongo brian <kayonbrian@gmail.com>
2022-06-09 01:45:29 +0300

    replacing a elemen

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list =[]
-    for elm in my_list:
-        if elm == search:
-            new_list.append(replace)
-        else: 
-            new_list.append(elm)
-        
-        return new_list
+    def replace_elem(elm):
+        return (replace if elm == search else elm)
+    return list(map(replace_elem, my_list))
