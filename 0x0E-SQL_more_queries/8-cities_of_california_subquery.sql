4f27a6bff03d0da0ad878dd07681ddf6d0ce7c13
kaybrian <kayonbrian@gmail.com>
2022-08-11 23:20:02 +0200

    worked on the must scripts

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- lists all cities of california that can be found in the database
+SELECT id, name FROM cities WHERE state_id = (
+  SELECT id
+  FROM states
+  WHERE name='California'
+) ORDER BY cities.id ASC;
