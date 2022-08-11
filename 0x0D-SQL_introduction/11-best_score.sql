1fbad1ca95a9841b43804215bb5d40784eb59003
kaybrian <kayonbrian@gmail.com>
2022-08-11 11:35:25 +0200

    fix on the scores

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- print all scores from the table where score >= 10 
-SELECT score, name from second_table where score >= 10;
+SELECT score, name FROM second_table WHERE score >= '10' ORDER BY score DESC;
