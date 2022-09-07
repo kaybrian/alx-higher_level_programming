c8fb502d52493c376ae7ce59b68fb6a9185462a0
kaybrian <kayonbrian@gmail.com>
2022-09-07 21:00:05 +0200

    added a new styling

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-const value = process.argv[2]
+const value = process.argv[2];
 
 if (value === undefined || isNaN(value)) {
-    console.log("Not A number")
-}else{
-    console.log(parseInt(value));
+  console.log('Not A number');
+} else {
+  console.log('My number :'.concat(' ', parseInt(value)));
 }
-
