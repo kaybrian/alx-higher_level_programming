aee575b66249c5910a3c3654d2d2001adb6e18e7
kaybrian <kayonbrian@gmail.com>
2022-09-11 23:30:25 +0200

    afix in no7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,7 +2,6 @@
 """
     A script that lists all State objects from the database hbtn_0e_6_usa
 """
-
 import sys
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
@@ -24,6 +23,6 @@ if __name__ == "__main__":
 
     # print all states
     for state in states:
-        print("{}: {}".format(state.id, state.name))
+        print(f"{state.id}: {state.name}")
 
     session.close()
