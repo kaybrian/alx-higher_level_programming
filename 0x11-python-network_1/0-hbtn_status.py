707bce34cc16c70ba156f85811b35c61d0012645
kayongo <kayonbrian@gmail.com>
2022-09-26 22:56:00 +0200

    can send a result and get a result

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,6 +9,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as request:
         response = request.read()
         print("Body response:")
-        print(f"\t- type: {type(response)}")
-        print(f"\t- content: {response}")
-        print(f"\t- utf8 content: {response.decode('UTF-8')}")
+        print(f"\t- type: {type(response)}$")
+        print(f"\t- content: {response}$")
+        print(f"\t- utf8 content: {response.decode('UTF-8')}$")
